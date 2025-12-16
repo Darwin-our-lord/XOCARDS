cac7e6e97fe6c58e4b0d1eab00a691749af9df6d
Darwin-our-lord <David@prebensen.dk>
2025-12-16 23:17:46 +0100

    display all cards in the deckbuilder :D

--- XOCards/Assets/Scripts/AllCardUI.cs
+++ XOCards/Assets/Scripts/AllCardUI.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class AllCardUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform cardsContainer;
+    public GameObject cardPrefab;
+    public Deck allCards;
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateAllCardDisplayVisuals()
     {
-        
+        foreach (Transform child in cardsContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Card card in allCards.deck)
+        {
+            GameObject cardObj = Instantiate(cardPrefab, cardsContainer);
+
+            CardDisplayUI display = cardObj.GetComponent<CardDisplayUI>();
+            if (display != null)
+            {
+                display.SetData(card);
+            }
+
+            DeckCardPicker cardSelect = cardObj.GetComponent<DeckCardPicker>();
+            if (cardSelect != null)
+            {
+                cardSelect.SetCard(card);
+            }
+        }
     }
 }
